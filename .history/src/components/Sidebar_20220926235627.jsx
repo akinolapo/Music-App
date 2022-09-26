fe37b9f0cafdc909db32272cf7af8769bb88cbbe
Peace Akinola <akinolapo@gmail.com>
2022-09-26 23:57:50 +0100

    sideBar done

--- .history/src/components/Sidebar_20220926235627.jsx
+++ .history/src/components/Sidebar_20220926235627.jsx
@@ -35,7 +35,7 @@ const Sidebar = () => {
       {/* Mobile Sidebar */}
       <div className="absolute md:hidden top-6 right-3">
         {mobileMenuOpen ? (
-          <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)} />
+          <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} />
         ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} />}
       </div>
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
